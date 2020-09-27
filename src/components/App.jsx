bc13449a31f595968f83ead26786c4507bf2a150
Thiago Rodrigues <thiagolopes27@hotmail.com>
2020-09-27 14:59:57 -0300

    Added create note and delete note functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
+import InputArea from "./InputArea";
 import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes";
+//import notes from "../notes";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      {notes.map(function (note) {
-        return <Note key={note.id} title={note.title} content={note.content} />;
+      <InputArea onAdd={addNote} />
+      {notes.map(function (note, index) {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNote}
+          />
+        );
       })}
       <Footer />
     </div>
---
+++ src/components/InputArea.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+
+function InputArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
+
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+  }
+
+  function expand() {
+    setExpanded(true);
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          onChange={handleChange}
+          value={note.title}
+          name="title"
+          placeholder="Title"
+        />
+        <textarea
+          onChange={handleChange}
+          value={note.content}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button type="button" onClick={submitNote}>
+          <AddIcon />
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default InputArea;
